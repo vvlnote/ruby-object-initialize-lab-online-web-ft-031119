43445fe6261fc1d5f7376e421c61b5dbe4ffc2c4
Joy Lin <vvlnote@hotmail.com>
2019-03-21 21:55:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,10 @@
 class Dog
-  @breed = "Mutt"
   def initialize(name, breed)
     @name = name
-    @breed = breed
+    if breed == nil
+      @breed = "Mutt"
+    else
+      @breed = breed
+    end
   end
 end
